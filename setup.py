348a80236603d3b1c2afbb84678edd50b577d683
rusty <golovinartem2004@gmail.com>
2023-07-19 23:05:25 +0400

    Loosen dependency version requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alembic-postgresql-enum',
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     author="RustyGuard",
     packages=find_packages('src'),
@@ -14,9 +14,11 @@ setup(
         'SQLAlchemy'
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
