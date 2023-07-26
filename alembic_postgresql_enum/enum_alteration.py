53d71018b6fb584f65884d08b30e70d9fd6d7dde
rusty <golovinartem2004@gmail.com>
2023-07-27 00:10:58 +0400

    optional keyword argument for op.sync_enum_values enum_values_to_rename - Helps when enum value is renamed but database already has entries with old value

--- alembic_postgresql_enum/enum_alteration.py
+++ alembic_postgresql_enum/enum_alteration.py
@@ -4,7 +4,7 @@ SQLAlchemy enums.
 
 """
 
-from typing import List, Tuple, Any
+from typing import List, Tuple, Any, Iterable, TYPE_CHECKING
 
 import alembic.autogenerate
 import alembic.operations.base
@@ -12,6 +12,9 @@ import alembic.operations.ops
 import sqlalchemy
 from alembic.autogenerate.api import AutogenContext
 from alembic.operations.ops import UpgradeOps
+
+if TYPE_CHECKING:
+    from sqlalchemy.engine import Connection
 
 from .get_enum_data import get_connection, EnumNamesToValues, EnumNamesToTableReferences
 
@@ -47,6 +50,45 @@ class SyncEnumValuesOp(alembic.operations.ops.MigrateOperation):
         )
 
     @classmethod
+    def _set_enum_values(cls, connection: 'Connection',
+                         schema: str,
+                         enum_name: str,
+                         new_values: List[str],
+                         affected_columns: 'List[Tuple[str, str]]',
+                         ):
+        enum_type_name = f"{schema}.{enum_name}"
+        temporary_enum_name = f"{enum_name}_old"
+
+        query_str = (
+            f"""ALTER TYPE {enum_type_name} RENAME TO {temporary_enum_name};"""
+            f"""CREATE TYPE {enum_type_name} AS ENUM({', '.join(f"'{value}'" for value in new_values)});"""
+        )
+
+        for table_name, column_name in affected_columns:
+            query_str += (
+                # fixme default is dropped but not being restored
+                f"""ALTER TABLE {table_name} ALTER COLUMN {column_name} DROP DEFAULT;"""
+                f"""ALTER TABLE {table_name} ALTER COLUMN {column_name} TYPE {enum_type_name} USING {column_name}::text::{enum_type_name};"""
+            )
+
+        query_str += f"""DROP TYPE {temporary_enum_name}"""
+
+        for q in query_str.split(';'):
+            connection.execute(sqlalchemy.text(q))
+
+    @classmethod
+    def _update_affected_columns(cls, connection: 'Connection',
+                                 schema: str,
+                                 affected_columns: 'List[Tuple[str, str]]',
+                                 enum_values_to_rename: 'Iterable[Tuple[str, str]]'
+                                 ):
+        for (old_value, new_value) in enum_values_to_rename:
+            for table_name, column_name in affected_columns:
+                connection.execute(sqlalchemy.text(f'''
+                    UPDATE {schema}.{table_name} SET {column_name} = '{new_value}' WHERE {column_name} = '{old_value}'
+                '''))
+
+    @classmethod
     def sync_enum_values(
             cls,
             operations,
@@ -54,6 +96,8 @@ class SyncEnumValuesOp(alembic.operations.ops.MigrateOperation):
             enum_name: str,
             new_values: List[str],
             affected_columns: 'List[Tuple[str, str]]' = None,
+            *,
+            enum_values_to_rename: 'Iterable[Tuple[str, str]]' = tuple()
     ):
         """
         Replace enum values with `new_values`
@@ -70,26 +114,27 @@ class SyncEnumValuesOp(alembic.operations.ops.MigrateOperation):
             List of columns that references this enum.
             First value is table_name,
             second value is column_name
+        :param enum_values_to_rename:
+            Iterable of tuples containing old_name and new_name
+            enum_values_to_rename=[
+                ('tree', 'three') # to fix typo
+            ]
         """
-        with get_connection(operations) as conn:
-            enum_type_name = f"{schema}.{enum_name}"
-            temporary_enum_name = f"{enum_name}_old"
+        with get_connection(operations) as connection:
+            if enum_values_to_rename:
+                for (old_value, _) in enum_values_to_rename:
+                    new_values.append(old_value)
+                cls._set_enum_values(connection, schema, enum_name, new_values, affected_columns)
 
-            query_str = (
-                f"""ALTER TYPE {enum_type_name} RENAME TO {temporary_enum_name};"""
-                f"""CREATE TYPE {enum_type_name} AS ENUM({', '.join(f"'{value}'" for value in new_values)});"""
-            )
+                cls._update_affected_columns(connection, schema, affected_columns, enum_values_to_rename)
 
-            for table_name, column_name in affected_columns:
-                query_str += (
-                    f"""ALTER TABLE {table_name} ALTER COLUMN {column_name} DROP DEFAULT;"""
-                    f"""ALTER TABLE {table_name} ALTER COLUMN {column_name} TYPE {enum_type_name} USING {column_name}::text::{enum_type_name};"""
-                )
+                for (old_value, _) in enum_values_to_rename:
+                    new_values.remove(old_value)
+                cls._set_enum_values(connection, schema, enum_name, new_values, affected_columns)
 
-            query_str += f"""DROP TYPE {temporary_enum_name}"""
+                return
 
-            for q in query_str.split(';'):
-                conn.execute(sqlalchemy.text(q))
+            cls._set_enum_values(connection, schema, enum_name, new_values, affected_columns)
 
     def to_diff_tuple(self) -> 'Tuple[Any, ...]':
         return self.operation_name, self.old_values, self.new_values, self.affected_columns
--- tests/test_sync_enum_values.py
+++ tests/test_sync_enum_values.py
@@ -1,10 +1,12 @@
-from alembic import op
+from typing import TYPE_CHECKING
+
+import sqlalchemy
 from alembic.operations import Operations
 from alembic.runtime.migration import MigrationContext
 
+from sqlalchemy.engine import Connection
+
 from alembic_postgresql_enum import get_defined_enums
-from alembic_postgresql_enum.enum_alteration import SyncEnumValuesOp
-from alembic_postgresql_enum.get_enum_data import TableReference
 from tests.schemas import get_schema_with_enum_variants, DEFAULT_SCHEMA, USER_STATUS_ENUM_NAME, USER_STATUS_COLUMN_NAME, \
     USER_TABLE_NAME
 
@@ -29,3 +31,38 @@ def test_sync_enum_values_with_new_value(connection: 'Connection'):
     assert defined == {
         USER_STATUS_ENUM_NAME: tuple(new_enum_variants)
     }
+
+
+def test_sync_enum_values_with_renamed_value(connection: 'Connection'):
+    old_enum_variants = ["active", "passive"]
+
+    database_schema = get_schema_with_enum_variants(old_enum_variants)
+    database_schema.create_all(connection)
+    connection.execute(sqlalchemy.text(f'''
+        INSERT INTO {USER_TABLE_NAME} ({USER_STATUS_COLUMN_NAME}) VALUES ('active'), ('passive')
+    '''))
+
+    new_enum_variants = old_enum_variants.copy()
+    new_enum_variants.remove('passive')
+    new_enum_variants.append('inactive')
+
+    mc = MigrationContext.configure(connection)
+    ops = Operations(mc)
+
+    ops.sync_enum_values(DEFAULT_SCHEMA, USER_STATUS_ENUM_NAME, new_enum_variants,
+                         ((USER_TABLE_NAME, USER_STATUS_COLUMN_NAME),),
+                         enum_values_to_rename=[
+                             ('passive', 'inactive')
+                         ])
+
+    defined = get_defined_enums(connection, DEFAULT_SCHEMA)
+
+    assert defined == {
+        USER_STATUS_ENUM_NAME: tuple(new_enum_variants)
+    }
+
+    users_entries = connection.execute(sqlalchemy.text(f'''
+        SELECT {USER_STATUS_COLUMN_NAME} FROM {USER_TABLE_NAME}
+    ''')).scalars().all()
+
+    assert users_entries == ['active', 'inactive']
