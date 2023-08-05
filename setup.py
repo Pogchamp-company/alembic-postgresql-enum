5330ae85cba1b7311bbcd70bc775eac3823592f1
rusty <golovinartem2004@gmail.com>
2023-08-05 10:18:40 +0400

    Bump package version to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alembic-postgresql-enum',
-    version='0.1.4',
+    version='0.1.5',
     license='MIT',
     author="RustyGuard",
     packages=find_packages('src'),
