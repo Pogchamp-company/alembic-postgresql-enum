6f99cd0a4f3b1f57b6b4ecd9c260ae164dc44e96
barrust <barrust@gmail.com>
2024-01-03 12:13:45 -0500

    fix tests

--- tests/sync_enum_values/test_rename_default_if_required.py
+++ tests/sync_enum_values/test_rename_default_if_required.py
@@ -34,10 +34,10 @@ def test_array_default_value_no_schema():
 # Tests with schema provided
 ##################################################
 
-def test_without_renames_no_schema():
-    old_default_value = "'passive'::order_status_old"
+def test_without_renames_with_schema():
+    old_default_value = "'passive'::test.order_status_old"
 
-    assert (rename_default_if_required(old_default_value, 'order_status', [], None)
+    assert (rename_default_if_required(old_default_value, 'order_status', [], "test")
             == "'passive'::order_status")
 
 
@@ -46,7 +46,7 @@ def test_with_renames_with_schema():
 
     assert (rename_default_if_required(old_default_value, 'order_status', [
         ('passive', 'inactive')
-    ], "test") == "'inactive'::order_status")
+    ], "test") == "'inactive'::test.order_status")
 
 
 def test_array_with_renames_with_schema():
@@ -54,7 +54,7 @@ def test_array_with_renames_with_schema():
 
     assert (rename_default_if_required(old_default_value, 'order_status', [
         ('passive', 'inactive')
-    ], "test") == """'{"inactive"}'::order_status""")
+    ], "test") == """'{"inactive"}'::test.order_status""")
 
 
 def test_array_default_value_with_schema():
