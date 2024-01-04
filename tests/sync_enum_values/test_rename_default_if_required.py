0d5dc9ebecba8cdb87c54eb81c94332af0c06ffc
Tyler Barrus <barrust@gmail.com>
2024-01-04 01:10:41 -0500

    Fix #53 Add schema to rename default
    * add schema when attempting to rename default, if present

--- tests/sync_enum_values/test_rename_default_if_required.py
+++ tests/sync_enum_values/test_rename_default_if_required.py
@@ -1,31 +1,64 @@
 from alembic_postgresql_enum.sql_commands.column_default import rename_default_if_required
 
 
-def test_without_renames():
+def test_without_renames_no_schema():
     old_default_value = "'passive'::order_status_old"
 
-    assert (rename_default_if_required(old_default_value, 'order_status', [])
+    assert (rename_default_if_required(old_default_value, 'order_status', [], None)
             == "'passive'::order_status")
 
 
-def test_with_renames():
+def test_with_renames_no_schema():
     old_default_value = "'passive'::order_status_old"
 
     assert (rename_default_if_required(old_default_value, 'order_status', [
         ('passive', 'inactive')
-    ]) == "'inactive'::order_status")
+    ], None) == "'inactive'::order_status")
 
 
-def test_array_with_renames():
+def test_array_with_renames_no_schema():
     old_default_value = """'{"passive"}'::order_status_old"""
 
     assert (rename_default_if_required(old_default_value, 'order_status', [
         ('passive', 'inactive')
-    ]) == """'{"inactive"}'::order_status""")
+    ], None) == """'{"inactive"}'::order_status""")
 
 
-def test_array_default_value():
+def test_array_default_value_no_schema():
     old_default_value = """'{}'::order_status_old[]"""
 
-    assert (rename_default_if_required(old_default_value, 'order_status', [])
+    assert (rename_default_if_required(old_default_value, 'order_status', [], None)
             == """'{}'::order_status[]""")
+    
+##################################################
+# Tests with schema provided
+##################################################
+
+def test_without_renames_with_schema():
+    old_default_value = "'passive'::test.order_status_old"
+
+    assert (rename_default_if_required(old_default_value, 'order_status', [], "test")
+            == "'passive'::test.order_status")
+
+
+def test_with_renames_with_schema():
+    old_default_value = "'passive'::test.order_status_old"
+
+    assert (rename_default_if_required(old_default_value, 'order_status', [
+        ('passive', 'inactive')
+    ], "test") == "'inactive'::test.order_status")
+
+
+def test_array_with_renames_with_schema():
+    old_default_value = """'{"passive"}'::test.order_status_old"""
+
+    assert (rename_default_if_required(old_default_value, 'order_status', [
+        ('passive', 'inactive')
+    ], "test") == """'{"inactive"}'::test.order_status""")
+
+
+def test_array_default_value_with_schema():
+    old_default_value = """'{}'::test.order_status_old[]"""
+
+    assert (rename_default_if_required(old_default_value, 'order_status', [], "test")
+            == """'{}'::test.order_status[]""")
