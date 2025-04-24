2b4cbd0b2d18c9b2b10f4257b342f71bc66da62b
Michele Bianchi <jazzinghen@gmail.com>
2025-04-24 11:00:17 +0900

    Add support for Alembic module prefix as well

--- alembic_postgresql_enum/operations/create_enum.py
+++ alembic_postgresql_enum/operations/create_enum.py
@@ -21,11 +21,12 @@ class CreateEnumOp(EnumLifecycleOp):
 def render_create_enum_op(autogen_context: AutogenContext, op: CreateEnumOp):
     assert autogen_context.dialect is not None
     sqlalchemy_module_prefix = autogen_context.opts.get("sqlalchemy_module_prefix", "sa.")
+    alembic_module_prefix = autogen_context.opts.get("alembic_module_prefix", "op.")
     if op.schema != autogen_context.dialect.default_schema_name:
         return f"""
-            {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}', schema='{op.schema}').create(op.get_bind())
+            {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}', schema='{op.schema}').create({alembic_module_prefix}get_bind())
             """.strip()
 
     return f"""
-        {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}').create(op.get_bind())
+        {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}').create({alembic_module_prefix}get_bind())
         """.strip()
--- alembic_postgresql_enum/operations/drop_enum.py
+++ alembic_postgresql_enum/operations/drop_enum.py
@@ -21,11 +21,12 @@ class DropEnumOp(EnumLifecycleOp):
 def render_drop_enum_op(autogen_context: AutogenContext, op: DropEnumOp):
     assert autogen_context.dialect is not None
     sqlalchemy_module_prefix = autogen_context.opts.get("sqlalchemy_module_prefix", "sa.")
+    alembic_module_prefix = autogen_context.opts.get("alembic_module_prefix", "op.")
     if op.schema != autogen_context.dialect.default_schema_name:
         return f"""
-            {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}', schema='{op.schema}').drop(op.get_bind())
+            {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}', schema='{op.schema}').drop({alembic_module_prefix}get_bind())
             """.strip()
 
     return f"""
-        {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}').drop(op.get_bind())
+        {sqlalchemy_module_prefix}Enum({', '.join(map(repr, op.enum_values))}, name='{op.name}').drop({alembic_module_prefix}get_bind())
         """.strip()
